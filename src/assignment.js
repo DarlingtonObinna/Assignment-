cdf3a17b9c05eaa93f7dabc7490cc602a5d9a8c7
DarlingtonObinna <108609074+DarlingtonObinna@users.noreply.github.com>
2022-07-08 16:20:16 +0100

    Update assignment.js

--- src/assignment.js
+++ src/assignment.js
@@ -13,10 +13,15 @@ const assignment = {};
  * @param {Array} arrayOfNumbers the array of numbers to sum
  * @returns number the sum of the numbers
  */
-function sumOfNumbers(arrayOfNumbers) {
+   function sumOfNumbers(arrayOfNumbers) {
+   let sum= 0;
+   for(let i= 0; i < arrayOfNumbers.length; i++){
+    sum+= arrayOfNumbers[i] 
+ } 
+    return sum;
     return arrayOfNumbers.length;
-}
-// assignment.sumOfNumbers = sumOfNumbers;
+  }
+ assignment.sumOfNumbers = sumOfNumbers;
 
 /**
  * Challenge - 2
